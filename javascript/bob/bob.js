239be8a889fa0a26d70b9dbfe7e3e45116fae8f4
Jason Oscar <joscar@Jasons-MacBook-Air.local>
2014-10-07 10:32:44 -0400

    Exercism exercises

---
+++ javascript/bob/bob.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+	if(typeof input == 'string'){
+		return 'Whatever.';
+	}
+//
+// YOUR CODE GOES HERE
+//
+};
+
+module.exports = Bob;
